1f55e987589e1568e2545637b66b9bc67ac80bd6
peteNeverSleeps <peterdiaz164@gmail.com>
2025-02-25 05:07:24 -0500

    added dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
 
     # Dealer Proxy Services
     path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
     path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_reviews'),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,16 +1,25 @@
-import LoginPanel from "./components/Login/Login"
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Register from './components/Register/Register';
-import Login from './components/Login/Login'; 
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-
-        <Route path="/register" element={<Register />} />
-
+      <Route path="/register" element={<Register />} />
+      {/* Route to list all dealers */}
+      <Route path="/dealers" element={<Dealers />} />
+      {/* Route to view a single dealer's details and reviews */}
+      <Route path="/dealer/:id" element={<Dealer />} />
+      {/* Route to post a review for a dealer */}
+      <Route path="/postreview/:id" element={<PostReview />} />
+      {/* Add additional routes (home, about, etc.) as needed */}
     </Routes>
   );
 }
+
 export default App;
